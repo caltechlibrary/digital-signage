cae5177af0c444cb8187da1d5a994369888c633e
R. S. Doiel <rsdoiel@gmail.com>
2017-03-24 11:38:48 -0700

    Quick Save

---
+++ library/index.md
@@ -0,0 +1,3 @@
+
+This is a placeholder where any library sign content will go around besides the list of poster to cycle.
+
--- mk-website.bash
+++ mk-website.bash
@@ -15,36 +15,41 @@ function softwareCheck() {
 }
 
 function MakePage () {
-    license="$1"
-    nav="$2"
-    content="$3"
-    html="$4"
+    nav="$1"
+    content="$2"
+    html="$3"
+    tmpl="$4"
     # Always use the latest compiled mkpage
     APP=$(which mkpage)
     if [ -f ./bin/mkpage ]; then
         APP="./bin/mkpage"
     fi
 
-    echo "Rendering $html"
+    echo "Generating $html"
     $APP \
-	"title=text:mkpage: An experimental template and markdown processor" \
+	"title=text:Caltech Library Digital Signage" \
         "nav=$nav" \
         "content=$content" \
 	    "sitebuilt=text:Updated $(date)" \
-        "copyright=markdown:$(cat $license)" \
-        page.tmpl > $html
+        $tmpl > $html
 }
 
 function MakeSignageSite() {
+    start=$(pwd)
     cd $DNAME
-    echo $(pwd)
+    for FNAME in $(find . -type f | grep -E '.md$'); do
+        MakePage nav.md $(basename $FNAME) $(basename $FNAME .md).html ../page.tmpl
+    done
+    cd $start
 }
 
 echo "Checking necessary software is installed"
 softwareCheck mkpage
 echo "Generating website index.html"
-MakePage LICENSE nav.md README.md index.html
+MakePage nav.md README.md index.html page.tmpl
+echo "Generate website license.html"
+MakePage nav.md "markdown:$(cat LICENSE)" license.html page.tmpl
+echo "Generate signage sites"
 for DNAME in $(find . -type d -depth 1 | grep -v '.git'); do
-    echo $(pwd)
     MakeSignageSite $DNAME
 done
